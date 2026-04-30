1ffe2b8564d95810b17ebb039b6e5d1c2543c63d
AlexdRL <alejandrodelriolop@gmail.com>
2026-04-30 11:58:57 +0200

    Creado GestorJugadores
    (cherry picked from commit c234b4b31015bda7ee70aa8da86232521ab852b8)

---
+++ SpaceLostPoo/src/spaceLost/Entidad/GestorJugadores.java
@@ -0,0 +1,73 @@
+package spaceLost.Entidad;
+
+import java.util.Map;
+import spaceLost.Entidad.Jugador;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class GestorJugadores {
+
+        private Map<String, Jugador> jugadores;
+        private final String RUTA_FICHERO = "jugadores.txt";
+
+        public GestorJugadores() {
+            jugadores = new HashMap<>();
+            cargarJugadores();
+        }
+
+        public Jugador addJugador(String nombre) {
+            if (jugadores.containsKey(nombre)) {
+                return jugadores.get(nombre);
+            }
+
+            Jugador nuevo = new Jugador(nombre);
+            jugadores.put(nombre, nuevo);
+            guardarTodos(); // evita duplicados
+            return nuevo;
+        }
+
+        private void cargarJugadores() {
+            File fichero = new File(RUTA_FICHERO);
+
+            if (!fichero.exists()) return;
+
+            try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {
+                String linea;
+
+                while ((linea = br.readLine()) != null) {
+                    String[] partes = linea.split(";");
+
+                    if (partes.length == 3) {
+                        String nombre = partes[0];
+                        int sala = Integer.parseInt(partes[1]);
+                        int puntuacion = Integer.parseInt(partes[2]);
+
+                        Jugador j = new Jugador(nombre, sala, puntuacion);
+                        jugadores.put(nombre, j);
+                    }
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error al cargar jugadores");
+            }
+        }
+
+        public void guardarTodos() {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(RUTA_FICHERO))) {
+
+                for (Jugador j : jugadores.values()) {
+                    bw.write(j.toString());
+                    bw.newLine();
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error al guardar jugadores");
+            }
+        }
+
+        public Jugador getJugador(String nombre) {
+            return jugadores.get(nombre);
+        }
+    }
+}
